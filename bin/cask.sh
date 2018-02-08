4ccbe026be58c6f49a993ed93c32ccae93e51d92
Ola Nilsson <ola.nilsson@gmail.com>
2018-02-08 23:40:26 +0100

    cask.sh: Remove dead code

--- bin/cask.sh
+++ bin/cask.sh
@@ -29,8 +29,6 @@ else
 	shift
 fi
 
-#set -x
-
 case $subcommand in
 	install|pkg-file|update|build|clean-elc|eval|exec-path|files|\
 	help|info|init|link|list|load-path|outdated|package|\
@@ -50,44 +48,3 @@ case $subcommand in
 		;;
 esac
 
-exit $?
-
-# Parse command line options.
-while getopts hvo: OPT; do
-    case "$OPT" in
-        h)
-            echo $USAGE
-            exit 0
-            ;;
-        v)
-            echo "`basename $0` version 0.1"
-            exit 0
-            ;;
-        o)
-            OUTPUT_FILE=$OPTARG
-            ;;
-        \?)
-            # getopts issues an error message
-            echo $USAGE >&2
-            exit 1
-            ;;
-    esac
-done
-
-# Remove the switches we parsed above.
-shift `expr $OPTIND - 1`
-
-# We want at least one non-option argument. 
-# Remove this block if you don't need it.
-if [ $# -eq 0 ]; then
-    echo $USAGE >&2
-    exit 1
-fi
-
-# Access additional arguments as usual through 
-# variables $@, $*, $1, $2, etc. or using this loop:
-for PARAM; do
-    echo $PARAM
-done
-
-# EOF
